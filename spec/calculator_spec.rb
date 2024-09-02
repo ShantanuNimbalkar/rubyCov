a5c8583553466bf2cc7d20a7bdb2aeba49af2f4f
shantanunimbalkar <shantanu.nimbalkar@embold.io>
2024-09-02 20:49:11 +0530

    added

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../Calculator'
+require 'spec_helper'
+
+RSpec.describe Calculator do
+  describe '#add' do
+    it 'adds two numbers' do
+      calculator = Calculator.new
+      result = calculator.add(2, 3)
+      expect(result).to eq(5)
+    end
+  end
+end
+
